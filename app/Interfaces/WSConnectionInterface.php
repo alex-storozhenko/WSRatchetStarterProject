aff1bd03eb7e246f10ac618fb4afa86fa8ad23ef
Alex Storozhenko <a.storozhenko@live.com>
2019-02-05 22:53:49 +0200

    Add access level to methods in WSConnectionInterface

--- app/Interfaces/WSConnectionInterface.php
+++ app/Interfaces/WSConnectionInterface.php
@@ -18,7 +18,7 @@ interface WSConnectionInterface extends RatchetConnection
      *
      * @return int
      */
-    function resourceId();
+    public function resourceId();
 
     /**
      * Get a unique connection identifier,
@@ -28,5 +28,5 @@ interface WSConnectionInterface extends RatchetConnection
      *
      * @return string
      */
-    function signature();
+    public function signature();
 }
