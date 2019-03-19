94760947308974686309e5168fbaa7163d3e40ec
Yan Datsiuk <yanek2357111317232931u@gmail.com>
2019-03-19 22:38:26 +0200

    added phpunit test for ArrayConnectionRepository

---
+++ tests/ArrayConnectionRepositoryTest.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class ArrayConnectionRepositoryTest extends TestCase
+{
+
+    public function testCannotBeComparedWithInvalidOperator(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $arrConnectionRepository = new \App\Repositories\ArrayConnectionRepository();
+        $arrConnectionRepository->whereResourceId(0,'*');
+    }
+
+}
